ac7558b8db94d081ff2de3bb58b8cc2c23c240e1
Abdulrahman Hamed <abdulrahman2002hamed@gmail.com>
2026-08-05 04:19:04 +0400

    Update page.tsx

--- app/battle/[roomId]/page.tsx
+++ app/battle/[roomId]/page.tsx
@@ -1,3 +1,10 @@
-export default function BattlePage() {
-  return <div>Loading...</div>;
+import BattleClient from "@/components/battle/BattleClient";
+
+interface Props {
+  params: Promise<{ roomId: string }>;
+}
+
+export default async function BattlePage({ params }: Props) {
+  const { roomId } = await params;
+  return <BattleClient roomId={roomId} />;
 }
