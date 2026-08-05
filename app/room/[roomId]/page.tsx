068f5b75c2dad2033da8b177ea5961871f890405
Abdulrahman Hamed <abdulrahman2002hamed@gmail.com>
2026-08-05 04:18:46 +0400

    Update page.tsx

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -1,3 +1,10 @@
-export default function RoomPage() {
-  return <div>Loading...</div>;
+import LobbyClient from "@/components/lobby/LobbyClient";
+
+interface Props {
+  params: Promise<{ roomId: string }>;
+}
+
+export default async function RoomPage({ params }: Props) {
+  const { roomId } = await params;
+  return <LobbyClient roomId={roomId} />;
 }
